f0a490c8a281414e168009a64dff750147752efc
Mytthew <dm.mateusz@gmail.com>
2024-10-10 22:51:40 +0200

    #2: Change some domain model fields to nullable.

--- SharboAPI.Domain/Models/Event.cs
+++ SharboAPI.Domain/Models/Event.cs
@@ -3,8 +3,8 @@ namespace SharboAPI.Domain.Models;
 public class Event
 {
 	public int Id { get; set; }
-	public string Text { get; set; }
-	public string Image { get; set; }
+	public string? Text { get; set; }
+	public string? Image { get; set; }
 	public DateTime CreationDate { get; set; }
 	public User CreatedBy { get; set; }
 	public int EventTypeId { get; set; }
