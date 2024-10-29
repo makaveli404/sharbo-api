3ba25e0d78bf3adcebd57bf7416f0dbf484a0405
BartTux <bartpot98@gmail.com>
2024-10-29 09:34:56 +0100

    #3: Add logging functionality. Config appsettings for dev & prod environment

--- SharboAPI/Program.cs
+++ SharboAPI/Program.cs
@@ -1,4 +1,5 @@
 using SharboAPI.Infrastructure.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,12 +7,17 @@ builder.Services.AddDatabaseConfiguration(builder.Configuration);
 builder.Services.AddInfrastructure();
 builder.Services.AddOpenApi();
 
+builder.Host.UseSerilog((context, config) => 
+	config.ReadFrom.Configuration(context.Configuration));
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
 {
 	app.MapOpenApi();
 }
+
+app.UseSerilogRequestLogging();
 
 app.UseHttpsRedirection();
 
