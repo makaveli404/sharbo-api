d0aecf28eb712a6fda13f769d08877ab533e3e99
Mytthew <dm.mateusz@gmail.com>
2025-06-27 14:29:55 +0200

    #27: Split subsequent methods.

--- SharboAPI.Application/Validators/CreateGroupDtoValidator.cs
+++ SharboAPI.Application/Validators/CreateGroupDtoValidator.cs
@@ -7,10 +7,17 @@ public class CreateGroupDtoValidator : AbstractValidator<CreateGroupDto>
 {
 	public CreateGroupDtoValidator()
 	{
-		RuleFor(x => x.Name).NotEmpty().WithErrorCode("Group name is required").MaximumLength(50).WithMessage("Group name is too long").NotNull();
+		RuleFor(x => x.Name)
+			.NotEmpty()
+			.WithErrorCode("Group name is required")
+			.MaximumLength(50)
+			.WithMessage("Group name is too long")
+			.NotNull();
+
 		RuleFor(x => x.ImagePath)
 			.Must(path => string.IsNullOrEmpty(path) || Uri.IsWellFormedUriString(path, UriKind.Absolute))
 			.WithMessage("ImagePath must be a valid URL");
+
 		RuleForEach(x => x.Participants)
 			.ChildRules(p =>
 			{
