7bac9fe38c046aa2bafc44dc098df72c3bf9a75b
Mytthew <dm.mateusz@gmail.com>
2025-06-27 14:35:29 +0200

    #27: Split methods.

--- SharboAPI.Application/Validators/UpdateGroupDtoValidator.cs
+++ SharboAPI.Application/Validators/UpdateGroupDtoValidator.cs
@@ -7,7 +7,13 @@ public class UpdateGroupDtoValidator : AbstractValidator<UpdateGroupDto>
 {
 	public UpdateGroupDtoValidator()
 	{
-		RuleFor(x => x.Name).NotEmpty().WithErrorCode("Group name is required").MaximumLength(50).WithMessage("Group name is too long").NotNull();
+		RuleFor(x => x.Name)
+			.NotEmpty()
+			.WithErrorCode("Group name is required")
+			.MaximumLength(50)
+			.WithMessage("Group name is too long")
+			.NotNull();
+
 		RuleFor(x => x.ImagePath)
 			.Must(path => string.IsNullOrEmpty(path) || Uri.IsWellFormedUriString(path, UriKind.Absolute))
 			.WithMessage("ImagePath must be a valid URL");
